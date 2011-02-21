546c629bd3e4334cbe8fc1ae7a7965f2e9f0e095
Mark Heiges <mheiges@gmail.com>
2011-02-21 19:09:14 +0000

    update init.rb version and CHANGELOG
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/Redmine@39087 735e2a04-f8fc-0310-8a1b-f2942603c481

--- plugins/projects_tree_view/branches/0.0.4-2/init.rb
+++ plugins/projects_tree_view/branches/0.0.4-2/init.rb
@@ -11,7 +11,7 @@ Redmine::Plugin.register :projects_tree_view do
   name 'Projects Tree and Priorities View plugin'
   author 'Chris Peterson - EuPathDB'
   description 'This is a Redmine plugin which will turn the projects page into a tree view; it also adds a tab called Priorities for a more detailed project view'
-  version '0.0.4-1'
+  version '0.0.4-2'
 
   menu :top_menu, :projectspis, { :controller => 'projectspis', :action => 'index' }, :caption => 'Devel/Infra Priorities'
   menu :top_menu, :datapis, { :controller => 'datapis', :action => 'index' }, :caption => 'Data Priorities'
