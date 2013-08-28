f11513f64a759daa59989ec0537f8310a6010848
Mark Heiges <mheiges@gmail.com>
2013-08-28 15:36:16 +0000

    branch redmine_eupathdb_view_customizations
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/Redmine@57907 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ plugins/redmine_eupathdb_view_customizations/branches/RM-1.4.4/init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_eupathdb_view_customizations do
+  name 'Redmine Eupathdb View Customizations plugin'
+  author 'EuPathDB'
+  description 'This plugin customizes views in Redmine'
+  version '0.0.1'
+  url 'http://eupathdb.org'
+  author_url 'http://eupathdb.org'
+end
